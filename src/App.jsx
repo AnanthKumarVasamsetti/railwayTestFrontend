ea74cf79be01e92faa44728acf49403f8107b619
Ananth Vasamsetti <avasamsetti@microsoft.com>
2025-03-18 21:15:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState, useState } from "react";
+import axios from "axios";
+
+function App() {
+    const [data, setData] = useState([]);
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        axios.get("https://railwaytest-production-087c.up.railway.app/api/data")
+            .then(response => setData(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    const handleSubmit = () => {
+        axios.post("https://railwaytest-production-087c.up.railway.app/api/data", { message })
+            .then(response => {
+                setData([...data, response.data.data]);
+                setMessage("");
+            });
+    };
+
+    return (
+        <div>
+            <h1>React + Express + MongoDB</h1>
+            <input
+                type="text"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Add Message</button>
+            <ul>
+                {data.map((item, index) => (
+                    <li key={index}>{item.message}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
